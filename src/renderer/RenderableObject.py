5c6b79487acb1f8a9a67fbb4ea2d263b9523caa8
Loïs <loisdupl42@gmail.com>
2019-12-22 16:38:01 +0100

    Renderable object completed Place an anchor on image and rotate it around, etc..

--- src/renderer/RenderableObject.py
+++ src/renderer/RenderableObject.py
@@ -1,4 +1,9 @@
+import math
+from enum import Enum
+
 import pygame
+
+import Utils
 
 
 def loadImage(imageName):
@@ -16,7 +21,6 @@ def loadImage(imageName):
 
 
 class RenderableObject(pygame.sprite.Sprite):
-
     """
         CONSTRUCT
     """
@@ -32,20 +36,48 @@ class RenderableObject(pygame.sprite.Sprite):
         self.current_width = self.current_image.get_width()
         self.current_height = self.current_image.get_height()
 
-        self.anchor_x = self.original_width/2
-        self.anchor_y = self.original_height/2
+        self.set_anchor(AnchorType.MIDDLE)
+        self.anchor_x = self.original_width / 2
+        self.anchor_y = self.original_height / 2
+
+        self.rect = self.current_image.get_rect()
+
+        self.render_anchor = False
+        self.render_border = False
 
     """
         METHODS
     """
 
-    def render(self, screen, x=0, y=0, angle=0):
-        # TODO Render
-        pass
+    def render(self, screen, x, y, angle=0):
+        screen.blit(self.current_image, self.rect)
+        self.compute_center_point(screen, x, y, angle)
+        if self.render_anchor:
+            pygame.draw.line(screen, pygame.Color(0, 0, 255), (x - 5, y), (x + 5, y))
+            pygame.draw.line(screen, pygame.Color(255, 0, 0), (x, y - 5), (x, y + 5))
+        if self.render_border:
+            pygame.draw.rect(screen, pygame.Color(0, 255, 0), self.rect, 1)
 
-    def compute_center_point(self):
-        # TODO Compute
-        pass
+    def compute_center_point(self, screen, x, y, angle):
+        self.set_current_image(pygame.transform.rotozoom(self.original_image, angle, 1))
+        self.rect = self.get_current_image().get_rect()
+
+        dx = self.original_width / 2 - self.anchor_x
+        dy = self.original_height / 2 - self.anchor_y
+        d = Utils.calculate_distance(self.anchor_x, self.anchor_y, self.anchor_x + dx, self.anchor_y + dy)
+
+        a = math.atan2(dy, dx)
+        a -= math.radians(angle)
+        # print("Dx:", dx, "| Dy:", dy, "| D:", d, "| Angle (radians):", a, "| Angle (degrees):", math.degrees(a))
+
+        nx = math.cos(a)*d
+        ny = math.sin(a)*d
+
+        if self.render_anchor:
+            pygame.draw.line(screen, pygame.Color(200, 66, 245), (x, y), (x + nx, y))
+            pygame.draw.line(screen, pygame.Color(200, 66, 245), (x + nx, y), (x + nx, y + ny))
+
+        self.rect.center = (x + nx, y + ny)
 
     """
         GETTERS AND SETTERS
@@ -58,6 +90,7 @@ class RenderableObject(pygame.sprite.Sprite):
         self.original_image = new_original_image
         self.original_width = new_original_image.get_width()
         self.original_height = new_original_image.get_height()
+        self.rect = self.current_image.get_rect()
 
     def get_original_width(self):
         return self.original_width
@@ -71,7 +104,7 @@ class RenderableObject(pygame.sprite.Sprite):
     def set_current_image(self, new_current_image):
         self.current_image = new_current_image
         self.current_width = new_current_image.get_width()
-        self.original_height = new_current_image.get_height()
+        self.curren_height = new_current_image.get_height()
 
     def get_current_width(self):
         return self.current_width
@@ -90,3 +123,56 @@ class RenderableObject(pygame.sprite.Sprite):
 
     def set_anchor_y(self, new_anchor_y):
         self.anchor_y = new_anchor_y
+
+    def set_anchor(self, anchor_type):
+        if anchor_type == AnchorType.MIDDLE:
+            self.set_anchor_x(self.get_original_width() / 2)
+            self.set_anchor_y(self.get_original_height() / 2)
+        elif anchor_type == AnchorType.TOP_LEFT:
+            self.set_anchor_x(0)
+            self.set_anchor_y(0)
+        elif anchor_type == AnchorType.TOP_MIDDLE:
+            self.set_anchor_x(self.get_original_width() / 2)
+            self.set_anchor_y(0)
+        elif anchor_type == AnchorType.TOP_RIGHT:
+            self.set_anchor_x(self.get_original_width())
+            self.set_anchor_y(0)
+        elif anchor_type == AnchorType.MIDDLE_RIGHT:
+            self.set_anchor_x(self.get_original_width())
+            self.set_anchor_y(self.get_original_height() / 2)
+        elif anchor_type == AnchorType.BOTTOM_RIGHT:
+            self.set_anchor_x(self.get_original_width())
+            self.set_anchor_y(self.get_original_height())
+        elif anchor_type == AnchorType.BOTTOM_MIDDLE:
+            self.set_anchor_x(self.get_original_width() / 2)
+            self.set_anchor_y(self.get_original_height())
+        elif anchor_type == AnchorType.BOTTOM_LEFT:
+            self.set_anchor_x(0)
+            self.set_anchor_y(self.get_original_height())
+        elif anchor_type == AnchorType.MIDDLE_LEFT:
+            self.set_anchor_x(0)
+            self.set_anchor_y(self.get_original_height() / 2)
+
+    def is_rendering_anchor(self):
+        return self.render_anchor
+
+    def set_rendering_anchor(self, state):
+        self.render_anchor = state
+
+    def is_rendering_border(self):
+        return self.render_border
+
+    def set_rendering_border(self, state):
+        self.render_border = state
+
+
+class AnchorType(Enum):
+    MIDDLE = 0
+    TOP_LEFT = 1
+    TOP_MIDDLE = 2
+    TOP_RIGHT = 3
+    MIDDLE_RIGHT = 4
+    BOTTOM_RIGHT = 5
+    BOTTOM_MIDDLE = 6
+    BOTTOM_LEFT = 7
+    MIDDLE_LEFT = 8
