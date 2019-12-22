e04c797959cbfd4edcceb37323bae01535065e48
Loïs <loisdupl42@gmail.com>
2019-12-22 16:37:03 +0100

    Function to calculate distance between two points

--- src/Utils.py
+++ src/Utils.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 
@@ -7,3 +8,7 @@ def get_current_time_millis():
 
 def map(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def calculate_distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
